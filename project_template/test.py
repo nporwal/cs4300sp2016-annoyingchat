3a2184f409b0c32a7befe63856cbff0bf13e5b69
Alin Barsan <alinbarsan@outlook.com>
2016-04-22 14:21:07 -0400

    temporary commit, not stable, continuing work on other pc

--- project_template/test.py
+++ project_template/test.py
@@ -1,24 +1,26 @@
 from .models import Docs
 import os
-import Levenshtein
+import random
 import json
 
 
-def read_file(n):
-	path = Docs.objects.get(id = n).address;
+def read_file(path):
+	#path = Docs.objects.get(id = n).address;
+	#above line gives error "no such table: project_template_docs"
 	file = open(path)
 	transcripts = json.load(file)
 	return transcripts
 
-def _edit(query, msg):
-    return Levenshtein.distance(query.lower(), msg.lower())
+def find_random(q):
+	quotes = read_file("jsons/new_quotes.json")
+	movies = read_file("jsons/new_movies.json")
+	result = []
+	r = random.randint(0,len(quotes))
+	result.append(quotes[r] + " - \"" + movies[r] + "\"")
+	return result
 
 def find_similar(q):
-	transcripts = read_file(1)
-	result = []
-	for transcript in transcripts:
-		for item in transcript:
-			m = item['text']
-			result.append(((_edit(q, m)), m))
+	return ["trivial algorithm placeholder"]
 
-	return sorted(result, key=lambda tup: tup[0])
+def find_final(q):
+	return ["final algorithm placeholder"]

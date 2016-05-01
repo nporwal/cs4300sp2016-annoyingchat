1b49abc3635da5af2711bc587519265d0b14490f
Bobby Porwal <nporwal@msn.com>
2016-05-01 14:56:52 -0400

    added debugging for testing on heroku

--- project_template/settings.py
+++ project_template/settings.py
@@ -23,8 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 
 # Application definition
 
@@ -48,7 +47,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'project_template.ProcessExceptionMiddleware'
 )
 
 ROOT_URLCONF = 'gettingstarted.urls'

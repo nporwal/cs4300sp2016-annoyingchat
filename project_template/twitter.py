6409ac7c64107f39bd08c471ffc3aaa62f3063bc
Alin Barsan <alinbarsan@outlook.com>
2016-05-02 14:43:13 -0400

    No longer relying purely on exception to catch replying to same tweet.  Now it checks before computing response

--- project_template/twitter.py
+++ project_template/twitter.py
@@ -1,6 +1,7 @@
 import tweepy
 from project_template import qf
-
+import os.path
+import io
 
 CONSUMER_KEY =          "hgxRhymigitU3bwHVf8jnNcP2"
 CONSUMER_SECRET =       "JcFNhLuiBtSqdvjKec1pwLiZyDzrY9IrlTJd8DEKjAO7qqkE9k"
@@ -11,22 +12,39 @@ def get_tweet():
 	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 	auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 	api = tweepy.API(auth)
-	 
+
+	seen_tweets = []
+	with open("jsons/seen_tweets.txt") as f:
+		seen_tweets = f.readlines()
+		seen_tweets = [line.strip() for line in seen_tweets]
+
 	twts = api.search(q="@annoying_chat")     
 	for s in twts:
 		text =  str(s.text)
 		if "ANC" in text and not (s.user.screen_name == "annoying_chat"):
 			text = text[(text.index("ANC") + len("ANC: ")):]
 			sn = s.user.screen_name
+			skip_tweet = False		
+			for t in seen_tweets:
+				if text + sn == t:
+					skip_tweet = True
+					print "Skipping tweet: %s" % text
+			if skip_tweet: continue
 			response = qf.find_final(text)[0][0]
 			m = "@%s %s" % (sn, response)
 			m = m[:140] if len(m) > 140 else m
 			try:				
 				s = api.update_status(m, s.id)
 				print "Found new tweet to reply to:\n%s" % text
+				seen_tweets.append(text + sn)
 			except:
 				pass
 				#do nothing if status is a duplicate
+
+	with open("jsons/seen_tweets.txt", 'w') as f:
+		for t in seen_tweets:
+			f.write(str(t) + "\n")
+
 	return "Processed Tweets."
 
 #Tried looking at random tweets from an incoming stream, but many were in various languages and/or did not fit the "chat" style.
